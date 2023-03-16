a6988e6eafad50aa3ed74429deef27baf4e3fa75
Alex Dragomir <alex.dragomir@caphyon.com>
2023-03-16 11:37:09 +0200

    Added animation control

---
+++ WinUI Presentation2/AnimationControl.xaml.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include "AnimationControl.xaml.h"
+#if __has_include("AnimationControl.g.cpp")
+#include "AnimationControl.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::WinUI_Presentation2::implementation
+{
+    AnimationControl::AnimationControl()
+    {
+        InitializeComponent();
+    }
+
+    int32_t AnimationControl::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void AnimationControl::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void AnimationControl::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        //myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ WinUI Presentation2/AnimationControl.xaml.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "winrt/Microsoft.UI.Xaml.h"
+#include "winrt/Microsoft.UI.Xaml.Markup.h"
+#include "winrt/Microsoft.UI.Xaml.Controls.Primitives.h"
+#include "AnimationControl.g.h"
+
+namespace winrt::WinUI_Presentation2::implementation
+{
+    struct AnimationControl : AnimationControlT<AnimationControl>
+    {
+        AnimationControl();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::WinUI_Presentation2::factory_implementation
+{
+    struct AnimationControl : AnimationControlT<AnimationControl, implementation::AnimationControl>
+    {
+    };
+}
